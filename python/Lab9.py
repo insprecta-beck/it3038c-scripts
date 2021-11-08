ded7aca1326ede569603fa360f3cd0924c07410d
Luke Beckwith <57149434+insprecta-beck@users.noreply.github.com>
2021-11-07 20:16:52 -0500

    Update Lab9.py

--- python/Lab9.py
+++ python/Lab9.py
@@ -5,5 +5,5 @@ r = requests.get('http://localhost:3000')
 aList = r.json()
 
 r.json = [{"name":"widget1","color":"blue"},{"name":"widget2","color":"green"},{"name":"widget3","color":"black"},{"name":"widgetX","color":"blue"}]
-aList = json.loads('s')
+aList = json.loads()
 print("The color and name of the widget is " (aList['name'][0]['color']))
